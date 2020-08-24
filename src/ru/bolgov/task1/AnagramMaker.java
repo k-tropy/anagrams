dc2fa0ad05374b886e3d9cbaea730fdad4786197
Bolgov Sergey <cerega6@yandex.ru>
2020-08-24 14:33:32 +0300

    Fixed errors on the merge request

---
+++ src/ru/bolgov/task1/AnagramMaker.java
@@ -0,0 +1,41 @@
+package ru.bolgov.task1;
+
+import java.util.StringJoiner;
+
+public class AnagramMaker {
+    static String makeAnagram(String text) {
+
+        StringJoiner result = new StringJoiner(" ");
+        String[] words = text.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+            result.add(reverseWord(words[i]));
+        }
+
+        return result.toString();
+    }
+
+    private static StringBuilder reverseWord(String inputWord) {
+        StringBuilder result = new StringBuilder("");
+        char[] letters = inputWord.toCharArray();
+        for (int i = 0, j = letters.length - 1; i < letters.length; i++, j--) {
+            if (i < j) {
+                if (!Alphabet.checkLetter(letters[i])) {
+                    j++;
+                    result.append(letters[i]);
+                    continue;
+                } else if (!Alphabet.checkLetter(letters[j])) {
+                    i--;
+                    continue;
+                } else {
+                    char v = letters[i];
+                    letters[i] = letters[j];
+                    letters[j] = v;
+                }
+            }
+            result.append(letters[i]);
+        }
+        return result;
+    }
+
+}
---
+++ src/ru/bolgov/task1/MainClass.java
@@ -0,0 +1,23 @@
+package ru.bolgov.task1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        String text = readFromConsole();
+        System.out.println(AnagramMaker.makeAnagram(text));
+    }
+
+    private static String readFromConsole() {
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            return (reader.readLine());
+        } catch (IOException e) {
+            System.out.println("Error read from console");
+        }
+        return "";
+    }
+}
