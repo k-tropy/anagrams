863f52337ec41b4b7cff004a3f36052560f1d6e9
Bolgov Sergey <cerega6@yandex.ru>
2020-08-27 20:32:22 +0300

    Added various tests

--- src/test/first_test/AnagramMakerTest.java
+++ src/test/first_test/AnagramMakerTest.java
@@ -1,5 +1,5 @@
 package first_test;
-package;
+
 
 import static org.junit.Assert.*;
 
@@ -9,28 +9,55 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import ru.bolgov.task1.AnagramMaker;
+
 public class AnagramMakerTest {
     private AnagramMaker anagram;
 
-    @Before
-    public void initTest() {
-        anagram = new AnagramMaker();
-    }
-
-    @After
-    public void afterTest() {
-        anagram = null;
-    }
-
     @Test
-    public void testMakeAnagramEasy() {
-        // assertEquals("asdvcl;", anagram.makeAnagram("skwalc;"));
+    public void testEasy() { 
+        anagram = new AnagramMaker();
         assertEquals("j", anagram.makeAnagram("j"));
     }
 
     @Test
-    public void testMakeAnagram() {
+    public void testWithSymbols() { 
+        anagram = new AnagramMaker();
         assertEquals("jpo;,vs", anagram.makeAnagram("svo;,pj"));
     }
+    
+    @Test
+    public void testWithSpace() { 
+        anagram = new AnagramMaker();
+        assertEquals("asdfg hjkl", anagram.makeAnagram("gfdsa lkjh"));
+    }
+    
+    @Test
+    public void testUpperCase() { 
+        anagram = new AnagramMaker();
+        assertEquals("QWERTY", anagram.makeAnagram("YTREWQ"));
+    }
+    
+    @Test
+    public void testWithNumbers() { 
+        anagram = new AnagramMaker();
+        assertEquals("jpo56,vs", anagram.makeAnagram("svo56,pj"));
+    }
+    
+    @Test
+    public void testRussian() { 
+        anagram = new AnagramMaker();
+        assertEquals("‚‡Ó‘›,ﬁ¡ÛÒ", anagram.makeAnagram("‚‡Ó‘›,ﬁ¡ÛÒ"));
+    }
+    
+    @Test
+    public void testEmptyInput() { 
+        anagram = new AnagramMaker();
+        assertEquals("", anagram.makeAnagram(""));
+    }
+    
+    
+//TODO testNullInput
+    
 
 }
